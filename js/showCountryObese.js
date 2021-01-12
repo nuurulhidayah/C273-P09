1fcd94c41f79f52adb949f0dcf49a365d052bbdb
Nuurulhidayah <19040408@myrp.edu.sg>
2021-01-12 14:01:27 +0800

    second commit

--- js/showCountryObese.js
+++ js/showCountryObese.js
@@ -1,26 +1,19 @@
 $(document).ready(function () {
     $("#idCountry").change(function () {
-        var population = $_GET['population'];
-        var obese = $_GET['obese'];
-        $.ajax({
-            type: "GET",
-            url: "http://localhost/C273/p09/showCountryObese.php",
-            cache: false,
-            data: "population=" + population + "&obese=" + obese,
-            dataType: "JSON",
-            success: function (response) {
-                var message = "<thead><tr><th>Population</th><th>Obese</th></tr></thead>";
-                message += "<tbody>";
-                for(i = 0; i < response.length; i++){
-                    message += "<tr><td>" + response[i].population + "</td>"
-                    + "<td>" + response[i].obese + "</td></tr>";
-                }
-                message += "</tbody>";
-                $("#obeseTable").html(message);
-            },
-            error: function (obj, textStatus, errorThrown) {
-                console.log("Error " + textStatus + ": " + errorThrown);
-            }
-        });
+        console.log($("#idCountry").val());
+        $.get("getCountryDetails.php", {
+            id: $("#idCountry").val()
+        }, function (data) {
+            console.log(data);
+            var msg = "<thead><tr><th>Population</th><th>Obese</th></tr></thead><tbody>";
+            data.forEach(i => {
+                msg += "<tr>";
+                msg += "<td>" + i.population + "</td>";
+                msg += "<td>" + i.obese + "</td>";
+                msg += "</tr>";
+            });
+            msg += "</tbody>";
+            $("#obeseTable").html(msg);
+        }, "json");
     });
 });
